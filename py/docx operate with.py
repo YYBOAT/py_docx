7930e573a174f1601b7a259cef85006d9895a17c
username <m1504747@gmail.com>
2025-07-09 10:37:20 +0800

    啊

--- py/docx operate with.py
+++ py/docx operate with.py
@@ -8,8 +8,10 @@ from docx import Document
 
 rawpath='E:\\codeproiect\\py1\\报告模板'
 testdate1='2024.07.17'
-dic_replace={'2023.06':'2024.07','2023.12':'2024.07','2023.07':'2024.07','2022':'2023'} #py字典，格式为key：value
-dic_year={'2023':'2024'}
+dic_replace={'2025.06.30':'2026.06.30'} #py字典，格式为key：value
+dic_year={'2024':'2025'}
+dic_strs={'GB 20052-2020':'GB 20052-2024'}
+dic_strs_1={''}
 dic_replace_data_after={'直流低电阻测试仪':'2024.07.17','游标卡尺':'2024.06.20','数字式测量投影仪':'2024.06.20',
                         '自然换气老化试验机':'2024.09.06','电子万能试验机':'2024.06.20','平头千分尺':'2024.06.20',
                         '绝缘电阻测试仪':testdate1,'直流电阻测试仪':testdate1,'全自动变比测试仪':testdate1,'功率分析仪':testdate1,
@@ -33,8 +35,13 @@ def document_text_replace(root,file_name,dic_):
                  para.runs[0].text= runstr.replace(key,dic_[key])  
           
     for table in document.tables:    #换表格
+       is_ok=0
        for row in table.rows:
+          if  is_ok==1:
+             break
           for cell in row.cells:
+            if  is_ok==1:
+              break
             for para in cell.paragraphs:
                for key,value in dic_.items():
                   if key in para.text:
@@ -44,7 +51,8 @@ def document_text_replace(root,file_name,dic_):
                        runstr+=run.text
                        para.runs[i].clear()
                
-                     para.runs[0].text= runstr.replace(key,dic_[key])  
+                     para.runs[0].text= runstr.replace(key,dic_[key]) 
+                     is_ok=1 
 
     for sec in document.sections:     #换页眉
        for table in sec.header.tables:
@@ -59,8 +67,8 @@ def document_text_replace(root,file_name,dic_):
                          runstr+=run.text
                          para.runs[i].clear()
                
-                    para.runs[0].text= runstr.replace(key,dic_[key])  
-    
+                       para.runs[0].text= runstr.replace(key,dic_[key])  
+     
     for sec in document.sections:     #换页脚
        for table in sec.footer.tables:
           for row in table.rows:
@@ -74,10 +82,10 @@ def document_text_replace(root,file_name,dic_):
                          runstr+=run.text
                          para.runs[i].clear()
                
-                         para.runs[0].text= runstr.replace(key,dic_[key])  
+                       para.runs[0].text= runstr.replace(key,dic_[key])  
    
     #new_file_name=file_name.replace('docx','doc') #改另存为doc，实际文件还是docx没用
-    new_file_name=file_name.replace('CF2023','CF2024')
+    new_file_name=file_name.replace('CFDS2024','CFDS2025')
     document.save(root+'\\'+new_file_name)
 
 
@@ -108,13 +116,14 @@ def document_text_replace_in_table(root,file_name,dic_,N_offset):
     document.save(root+'\\'+file_name) #file_name.replace('docx','doc')用来保存成doc文件，但是没用
 
 
-for root,dirs,files in os.walk('E:\\codeproiect\\py1\\报告模板'):
+for root,dirs,files in os.walk('E:\\codeproiect\\py1\\temp1'):
    for file in files:
        # 找出文件中以.doc结尾并且不以~$开头的文件(~S是为了排除临时文件的)
        if file.endswith('.docx') and not file.startswith( '.$'):
           print(file)
-          document_text_replace(root,file,dic_year) 
-          #document_text_replace_in_table(root,file,dic_replace_data_after,5)
+          document_text_replace(root,file,dic_replace) 
+        
+          #document_text_replace_in_table(root,file,dic_strs,1)
    
 
 
